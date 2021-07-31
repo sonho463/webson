3040f4da11b3d1ad0265de0fb5c0a8469211e16b
sonho463 <son.ho463@gmail.com>
2021-07-31 15:10:19 +0900

    アニメーションOK？

--- scripts/main.js
+++ scripts/main.js
@@ -60,14 +60,33 @@ const stagger = () => {
 
 TweenMax.set(".textAnimate", {
   opacity: "0",
+	y:"50"
 });
 
 const textAnimate = () => {
-	TweenMax.to(".textAnimate", 2, {
+	TweenMax.to(".textAnimate", 1, {
     opacity: "1",
+		y:"0"
   });
 };
+
+const hamburgerAnimate=()=>{
+	TweenMax.to(".hamburger",5,{
+		rotation:"360",
+		repeat: -1,
+		yoyo: true
+	})
+}
+
+const headerNav = document.getElementById('header-nav');
+const btnHamburger = document.getElementById('hamburger');
+
+hamburger.addEventListener('click',function(){
+	headerNav.classList.toggle('open');
+})
+
 
 setTimeout(stagger, 1000);
 setTimeout(arrowAnimate, 2500);
 setTimeout(textAnimate, 3500);
+setTimeout(hamburgerAnimate, 3500);
