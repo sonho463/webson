b6067b2130f87faadfddf0138b04bf781f5bdd97
sonho463 <son.ho463@gmail.com>
2021-08-12 10:56:25 +0900

    モーダル完成。地道な作業で

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 const lyConts = document.querySelectorAll(".ly_cont");
-console.log(lyConts);
+// console.log(lyConts);
 const cb = function (entries, observer) {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -22,8 +22,8 @@ const options = {
   threshold: 0.5,
 };
 
-const io = new IntersectionObserver(cb, options);
-lyConts.forEach((lyCont) => io.observe(lyCont));
+// const io = new IntersectionObserver(cb, options);
+// lyConts.forEach((lyCont) => io.observe(lyCont));
 
 // heroHeadingのアニメーション
 
@@ -60,116 +60,161 @@ const stagger = () => {
 
 TweenMax.set(".textAnimate", {
   opacity: "0",
-	y:"50"
+  y: "50",
 });
 
 const textAnimate = () => {
-	TweenMax.to(".textAnimate", 1, {
+  TweenMax.to(".textAnimate", 1, {
     opacity: "1",
-		y:"0"
+    y: "0",
   });
 };
 
-const hamburgerAnimate=()=>{
-	TweenMax.to(".hamburger",5,{
-		rotation:"360",
-		repeat: -1,
-		yoyo: true
-	})
-}
+const hamburgerAnimate = () => {
+  TweenMax.to(".hamburger", 5, {
+    rotation: "360",
+    repeat: -1,
+    yoyo: true,
+  });
+};
 
-const headerNav = document.getElementById('header-nav');
-const btnHamburger = document.getElementById('hamburger');
+const headerNav = document.getElementById("header-nav");
+const btnHamburger = document.getElementById("hamburger");
 
-hamburger.addEventListener('click',function(){
-	headerNav.classList.toggle('open');
-})
-
+hamburger.addEventListener("click", function () {
+  headerNav.classList.toggle("open");
+});
 
 setTimeout(stagger, 1000);
 setTimeout(arrowAnimate, 2500);
 setTimeout(textAnimate, 3500);
 setTimeout(hamburgerAnimate, 3500);
 
-
 //　実績ブロックのモーダルウィンドウの動作
 
-
-
 // DOM設定
-const techmeets 	= document.querySelector('#techmeets');
-const studyLink01 = document.querySelector('#study-link01');
-const studyLink02 = document.querySelector('#study-link02');
-const bochi 			= document.querySelector('#bochi');
-const ytu 				= document.querySelector('#ytu');
+const techmeets = document.querySelector("#techmeets");
+const studyLink01 = document.querySelector("#study-link01");
+const studyLink02 = document.querySelector("#study-link02");
+const bochi = document.querySelector("#bochi");
+const ytu = document.querySelector("#ytu");
 
-const worksArray = document.querySelectorAll('.bl_work');
-const overLay 		= document.querySelector('#over-lay');
+const worksArray = document.querySelectorAll(".bl_work");
+const overLay = document.querySelector("#over-lay");
 
 function ScrollWindow(elem) {
-	var element = document.getElementById(elem);
-	var rect = element.getBoundingClientRect();
-	var elemtop = rect.top + window.pageYOffset;
-	// document.documentElement.scrollTop = elemtop;
-	document.documentElement.scrollTo({
-		top: elemtop,
-		behavior: 'smooth'
-	})
+  var element = document.getElementById(elem);
+  var rect = element.getBoundingClientRect();
+  var elemtop = rect.top + window.pageYOffset;
+  // document.documentElement.scrollTop = elemtop;
+  document.documentElement.scrollTo({
+    top: elemtop,
+    behavior: "smooth",
+  });
 }
-
-
 
 // モーダル開く
 const addModalToTechmeets = () => {
-	ScrollWindow('techmeets');
-	techmeets.classList.add('modal');
-	overLay.classList.remove('hidden')
-}
-techmeets.addEventListener('click', addModalToTechmeets);
+  ScrollWindow("techmeets");
+  techmeets.classList.add("modal");
+  overLay.classList.remove("hidden");
+};
+techmeets.addEventListener("click", addModalToTechmeets);
 
 const addModalToStudyLink01 = () => {
-	ScrollWindow('study-link01');
-	studyLink01.classList.add('modal');
-	overLay.classList.remove('hidden');
-}
-studyLink01.addEventListener('click', addModalToStudyLink01);
+  ScrollWindow("study-link01");
+  studyLink01.classList.add("modal");
+  overLay.classList.remove("hidden");
+};
+studyLink01.addEventListener("click", addModalToStudyLink01);
 
 const addModalToStudyLink02 = () => {
-	ScrollWindow('study-link02');
-	studyLink02.classList.add('modal');
-	overLay.classList.remove('hidden');
-}
-studyLink02.addEventListener('click', addModalToStudyLink02);
+  ScrollWindow("study-link02");
+  studyLink02.classList.add("modal");
+  overLay.classList.remove("hidden");
+};
+studyLink02.addEventListener("click", addModalToStudyLink02);
 
 const addModalToBochi = () => {
-	ScrollWindow('bochi');
-	bochi.classList.add('modal');
-	overLay.classList.remove('hidden');
-}
-bochi.addEventListener('click', addModalToBochi);
+  ScrollWindow("bochi");
+  bochi.classList.add("modal");
+  overLay.classList.remove("hidden");
+};
+bochi.addEventListener("click", addModalToBochi);
 
 const addModalToYtu = () => {
-	ScrollWindow('ytu');
-	ytu.classList.add('modal');
-	overLay.classList.remove('hidden');
-}
-ytu.addEventListener('click', addModalToYtu);
+  ScrollWindow("ytu");
+  ytu.classList.add("modal");
+  overLay.classList.remove("hidden");
+  console.log("開く");
+};
 
+ytu.addEventListener("click", addModalToYtu);
 
+// 閉じるボタンでモーダル閉じる
+const btnCloseTechmeets = techmeets.querySelector("#btn_close");
+const btnCloseStudyLink01 = studyLink01.querySelector("#btn_close");
+const btnCloseStudyLink02 = studyLink02.querySelector("#btn_close");
+const btnCloseBochi = bochi.querySelector("#btn_close");
+const btnCloseYtu = ytu.querySelector("#btn_close");
 
-
-// モーダル閉じる
-const closeModal = () => {
-	console.log('start');
-	console.log(worksArray);
-	worksArray.forEach((work)=>{
-		console.log(work)
-		if(work.classList.contains('modal')){
-			console.log(work.classList.contains('modal'))
-			work.classList.remove('modal');
-		}
-	})
+const closeTechmeets = (event) => {
+	event.stopPropagation();
+	if(techmeets.classList.contains('modal')){
+		techmeets.classList.remove('modal')
+	}
 	overLay.classList.add('hidden');
-}
+};
+btnCloseTechmeets.addEventListener("click", closeTechmeets);
 
-overLay.addEventListener('click', closeModal)
+const closeStudyLink01 = (event) => {
+	event.stopPropagation();
+	if(studyLink01.classList.contains('modal')){
+		studyLink01.classList.remove('modal')
+	}
+	overLay.classList.add('hidden');
+};
+btnCloseStudyLink01.addEventListener("click", closeStudyLink01);
+
+const closeStudyLink02 = (event) => {
+	event.stopPropagation();
+	if(studyLink02.classList.contains('modal')){
+		studyLink02.classList.remove('modal')
+	}
+	overLay.classList.add('hidden');
+};
+btnCloseStudyLink02.addEventListener("click", closeStudyLink02);
+
+const closeBochi = (event) => {
+	event.stopPropagation();
+	if(bochi.classList.contains('modal')){
+		bochi.classList.remove('modal')
+	}
+	overLay.classList.add('hidden');
+};
+btnCloseBochi.addEventListener("click", closeBochi);
+
+const closeYtu = (event) => {
+	event.stopPropagation();
+	if(ytu.classList.contains('modal')){
+		ytu.classList.remove('modal')
+	}
+	overLay.classList.add('hidden');
+};
+btnCloseYtu.addEventListener("click", closeYtu);
+
+// 周りをクリックしたらモーダル閉じる
+const closeModal = (event) => {
+  event.stopPropagation();
+  worksArray.forEach((work) => {
+    console.log(work);
+    if (work.classList.contains("modal")) {
+      console.log("とじる");
+      console.log(work.classList.contains("modal"));
+      work.classList.remove("modal");
+    }
+  });
+  overLay.classList.add("hidden");
+};
+
+overLay.addEventListener("click", closeModal);
